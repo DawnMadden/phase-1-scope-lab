03cbbc6f4d35697da0cfde6445250ab6c94c66a4
Dawn Madden <coachdawnmadden@gmail.com>
2023-09-10 16:31:33 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+// upperCaseCustomerName();
+// console.log(customerName);
+}   
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'dog';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jim';
+    console.log(leastFavoriteCustomer);
+}
+
+
